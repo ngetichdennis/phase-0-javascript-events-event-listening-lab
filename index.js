64b6f37442214d37e107ee2c5dbca6541cbbb317
Dennis <ngetichdennis90@gmail.com>
2024-01-08 16:16:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const input=document.querySelector("#input");
+    input.textContent=true
+    input.addEventListener('input',() =>{
+        console.log(true)
+    })
+    
+    }
+
+    
